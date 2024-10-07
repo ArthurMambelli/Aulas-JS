0d9d75fd56bbcd14cf64a8fd8dd61a401247c315
Arthur Mambelli <arthurmambelli@gmail.com>
2024-10-07 15:56:29 -0300

    Alterações

---
+++ Exercícios DOM 2/_js/main.js
@@ -0,0 +1,9 @@
+function verGatos() {
+  document.getElementById("img1").style.display = "block";
+}
+function verCachorros() {
+  document.getElementById("img2").style.display = "block";
+}
+function verPassaros() {
+  document.getElementById("img3").style.display = "block";
+}
